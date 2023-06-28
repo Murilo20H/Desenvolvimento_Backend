a8898ed78ef84f76eba665a40e1deba6d65b0b9f
Murilo20H <murilohenrique123correa@gmail.com>
2023-06-27 23:26:59 -0300

    Listas e aulas

---
+++ Listas/Lista_2/lista2-exercicio1/src/main/java/com/programando/lista2exercicio1/Lista2Exercicio1Application.java
@@ -0,0 +1,13 @@
+package com.programando.lista2exercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista2Exercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista2Exercicio1Application.class, args);
+	}
+
+}
---
+++ Listas/Lista_2/lista2-exercicio1/src/test/java/com/programando/lista2exercicio1/Lista2Exercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programando.lista2exercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista2Exercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
